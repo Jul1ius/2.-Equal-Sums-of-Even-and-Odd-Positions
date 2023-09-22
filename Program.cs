a8e20e94d8bd50798df4a821a2a675a879dd9c47
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-22 11:22:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2._Equal_Sums_of_Even_and_Odd_Positions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                String currentNum = i.ToString();
+
+                int oddSum = 0;
+                int evenSum = 0;
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    Char c = currentNum[j];
+                    int currentDigit = int.Parse(c.ToString());
+
+                    if (j % 2 == 0)
+                        oddSum += currentDigit;
+                    else
+                        evenSum += currentDigit;
+                }
+
+                if (oddSum == evenSum)
+                    Console.Write(i + " ");
+            }
+        
+        }
+    }
+}
